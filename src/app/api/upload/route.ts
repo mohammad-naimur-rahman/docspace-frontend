c69c55dbae45b73c373e8d8e0145f26d466dd447
Naimur <naeemhasan28@gmail.com>
2023-12-27 18:36:54 +0600

    file upload and show basic view completed

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,27 @@
+import { mkdir, writeFile } from 'fs/promises'
+import { NextRequest, NextResponse } from 'next/server'
+import path from 'path'
+
+export async function POST(request: NextRequest) {
+  const data = await request.formData()
+  const file: File | null = data.get('file') as unknown as File
+
+  if (!file) {
+    return NextResponse.json({ success: false })
+  }
+
+  const bytes = await file.arrayBuffer()
+  const buffer = Buffer.from(bytes)
+
+  const filesFolderPath = path.join(process.cwd(), 'public', 'files')
+
+  await mkdir(filesFolderPath, { recursive: true })
+
+  console.log(file)
+
+  const filePath = path.join(filesFolderPath, file.name)
+  await writeFile(filePath, buffer)
+  const relativePath = path.relative(path.join(process.cwd(), 'public'), filePath)
+
+  return NextResponse.json({ success: true, path: relativePath, size: file.size, type: file.type, name: file.name })
+}
